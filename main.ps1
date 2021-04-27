5e7df29429ff4b9a4a87405762a20b1b2ea8ec5e
EP-coode <68182419+EP-coode@users.noreply.github.com>
2021-04-27 15:50:50 +0200

    Handling OU added

--- main.ps1
+++ main.ps1
@@ -1,6 +1,28 @@
-﻿
-$users=Import-CSV users.csv
+﻿$users=Import-CSV users.csv
+
 Foreach ($i in $users) {
-    Write-Host ($i.FirstName+" "+$i.LastName)
-    New-ADUser -Name ($i.FirstName+" "+$i.LastName) -SamAccountName ($i.FirstName.ToLower()[0]+$i.LastName.ToLower()) -UserPrincipalName ($i.FirstName.ToLower()+"."+$i.LastName.ToLower()+"@contoso.com") -Path ("ou="+$i.Department+",dc=contoso,dc=com") -PasswordNeverExpires $true -ChangePasswordAtLogon $false -AccountPassword (ConvertTo-SecureString -AsPlainText "P@ssword901" -Force) -Enabled $true
+    $name = $i.FirstName+" "+$i.LastName
+    $sam_name = $i.FirstName.ToLower()[0]+$i.LastName.ToLower()
+    $upn_name = $i.FirstName.ToLower()+"."+$i.LastName.ToLower()+"@contoso.com"
+    # to rozwiązanie nie jest dobre dla zagnieżdzeń ou
+    $path = "ou="+$i.Department+",dc=contoso,dc=com"
+    $passwd = "P@ssword901"
+
+    $user_exist = Get-ADUser -Filter "SamAccountName -eq '$sam_name'"
+
+    If($user_exist -eq $null)
+    {
+        $ou_exist = Get-ADOrganizationalUnit -Filter "distinguishedName -eq '$path'"
+
+        If($ou_exist -eq $null){
+            New-ADOrganizationalUnit -Name $i.Department -Path "dc=contoso,dc=com"
+        }
+
+        New-ADUser -Name $name -SamAccountName $sam_name -UserPrincipalName $upn_name -Path $path -PasswordNeverExpires $true -ChangePasswordAtLogon $false -AccountPassword (ConvertTo-SecureString -AsPlainText $passwd -Force) -Enabled $true
+        Write-Host ("Message: "+$i.FirstName+" "+$i.LastName+"-ADDED") -ForegroundColor Green
+    }
+    Else
+    {
+        Write-Host ("WARNING: "+$i.FirstName+" "+$i.LastName+"------ALREADY EXIST") -ForegroundColor Yellow
+    }
 }
