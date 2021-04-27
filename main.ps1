92ae745ad396d786f5505c244b2be96938213785
EP-coode <68182419+EP-coode@users.noreply.github.com>
2021-04-27 15:21:14 +0200

    implement adding users

---
+++ main.ps1
@@ -0,0 +1,6 @@
+﻿
+$users=Import-CSV users.csv
+Foreach ($i in $users) {
+    Write-Host ($i.FirstName+" "+$i.LastName)
+    New-ADUser -Name ($i.FirstName+" "+$i.LastName) -SamAccountName ($i.FirstName.ToLower()[0]+$i.LastName.ToLower()) -UserPrincipalName ($i.FirstName.ToLower()+"."+$i.LastName.ToLower()+"@contoso.com") -Path ("ou="+$i.Department+",dc=contoso,dc=com") -PasswordNeverExpires $true -ChangePasswordAtLogon $false -AccountPassword (ConvertTo-SecureString -AsPlainText "P@ssword901" -Force) -Enabled $true
+}
